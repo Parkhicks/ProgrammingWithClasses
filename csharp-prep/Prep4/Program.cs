5ea4f8321f8008dfaba0bb2874dfe5e1a7ee65b6
Parkhicks <hic21003@byui.edu>
2023-09-19 18:42:46 -0600

    finished prep #4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,16 +5,28 @@ class Program
     static void Main(string[] args)
     {
         List<int> numbers =  new List<int>();
-        int num = 0;
+        int start = 0;
+        Console.WriteLine("Enter a number numbers you want to add to your list ");
+        Console.WriteLine("Enter a your first number: ");
         string number = Console.ReadLine();
-        Console.WriteLine("Enter a number: ");
-        num = int.Parse(number);
-        numbers.Add(num);
-        while (num != 0);
+        while (start == 0)
         {
+            int num = int.Parse(number);
+            if (num == 0)
+            {
+                start = 1;
+            }
+            else
+            {
+                numbers.Add(num);
+                Console.WriteLine("Enter another number, enter 0 if you're done");
+                number = Console.ReadLine();
+            }
             
-        }
-
+        };
+        Console.WriteLine($"The sum of those numbers is: {numbers.Sum()}");
+        Console.WriteLine($"The average of those numbers is: {numbers.Average()}");
+        Console.WriteLine($"The maximum of those numbers is: {numbers.Max()}");
 
     }
 }
