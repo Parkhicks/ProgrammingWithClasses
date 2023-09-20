b762660f2cf6cb29af79552f4f56ef6dd3da7ac1
Parkhicks <hic21003@byui.edu>
2023-09-20 16:42:05 -0600

    prep 5 start

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Globalization;
 
 class Program
 {
-    static void Main(string[] args)
+
+    void DisplayWelcome()
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Hello and welcome to the program!");
+    }
+    string PromptUserName()
+    {
+        Console.WriteLine("What is your name? ");
+        string uname = Console.ReadLine();
+        return uname;
+    } 
+    int PromptUserNum()
+    {
+        Console.WriteLine("What is your favorite number? ");
+        string nums = Console.ReadLine();
+        int num = int.Parse(nums);
+        return num;
+    }
+    int squarednum(int num)
+    {
+        int squarenumber = num * num;
+        return squarenumber;
+    }
+    void result(int num, string name)
+    {
+        Console.WriteLine($"{name}, the square of your number is {num} ");
     }
 }
