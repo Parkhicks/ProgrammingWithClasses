8959d8e1c0c8527069e1d247ce523c3d69b2327c
Parkhicks <hic21003@byui.edu>
2023-09-27 19:54:43 -0600

    did preparation

---
+++ Homework/Resume/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Job job1 = new Job();
+job1._jobTitle = "Software Engineer";
+job1._company = "Microsoft";
+job1._startYear = 2019;
+job1._endYear = 2022;
+Job job2 = new Job();
+job2._company = "Apple";
+job2._jobTitle = "Manager";
+job2._startYear = 2017;
+job2._endYear = 2021;
+
+Resume myResume = new Resume();
+myResume._name = "Allison Rose";
+
+myResume._jobs.Add(job1);
+myResume._jobs.Add(job2);
+
+myResume.Display();
