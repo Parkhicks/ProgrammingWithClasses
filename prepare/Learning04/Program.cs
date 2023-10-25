3a8679fb446f64e1e8b8b6f08c1152e0f3f0c608
Parkhicks <hic21003@byui.edu>
2023-10-25 17:05:20 -0600

    Learning Prep Working

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+
+        Assignment test = new Assignment("Parker Hicks", "Religion");
+        string status = test.GetSummary();
+        Console.WriteLine(status);
     }
+
+    
 }
