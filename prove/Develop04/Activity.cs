819deffe7d6b703644557ee479b3ed3680b59a4d
Parkhicks <hic21003@byui.edu>
2023-10-31 16:00:41 -0600

    added devlop

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,17 @@
+public class Activity
+{
+    protected string _adescription = "";
+    protected string _name = "";
+    private string _welcome = "";
+    private string _goodbye = "";
+
+
+    public Activity(string name, string description)
+    {
+        _name = name;
+        _adescription = description;
+    }
+
+
+
+}
