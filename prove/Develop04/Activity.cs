4273c1881e567650d2d4528276dbdbb557734d22
Parkhicks <hic21003@byui.edu>
2023-11-04 19:49:28 -0600

    Finished up the Activities program

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,17 +1,75 @@
-public class Activity
-{
-    protected string _adescription = "";
-    protected string _name = "";
-    private string _welcome = "";
-    private string _goodbye = "";
+public class Activity {
+    protected string _name;
+    protected string _description;
+    protected int _time;
+    protected int _mathTime;
 
-
-    public Activity(string name, string description)
-    {
+    public Activity(string name, string description){
         _name = name;
-        _adescription = description;
+        _description = description;
+    }
+    public void StartBace(){
+        Console.Clear();
+        spin();
+        Console.Clear();
+        Console.WriteLine($"{_name}: {_description}");
+        Console.WriteLine("========================================");
+        Console.WriteLine();
+        Console.WriteLine($"You picked the {_name} activity. How long do you want to do it for?");
+        Console.Write("Put time in seconds Ex. 10 : ");
+        _time = int.Parse(Console.ReadLine());
+        _mathTime = _time * 1000;
+        Console.WriteLine();
     }
 
+    public void EndActivity() {
+        Console.WriteLine();
+        Console.WriteLine($"You did the {_name} activity for {_time} seconds!");
+        Console.WriteLine();
 
-
+    }
+    public void countDown(){
+        Console.Write("5");
+        Thread.Sleep(1000);
+        Console.Write("\b \b");
+        Console.Write("4");
+        Thread.Sleep(1000);
+        Console.Write("\b \b");
+        Console.Write("3");
+        Thread.Sleep(1000);
+        Console.Write("\b \b");
+        Console.Write("2");
+        Thread.Sleep(1000);
+        Console.Write("\b \b");
+        Console.Write("1");
+        Thread.Sleep(1000);
+        Console.Write("\b \b");
+        Console.Write("0");
+        Console.Write("\b \b");
+    }
+    public void spin(){
+        Console.Write("|");
+        Thread.Sleep(100);
+        Console.Write("\b \b");
+        Console.Write("/");
+        Thread.Sleep(100);
+        Console.Write("\b \b");
+        Console.Write("-");
+        Thread.Sleep(100);
+        Console.Write("\b \b");
+        Console.Write("\\");
+        Thread.Sleep(100);
+        Console.Write("\b \b");
+        Console.Write("|");
+        Thread.Sleep(100);
+        Console.Write("\b \b");
+        Console.Write("/");
+        Thread.Sleep(100);
+        Console.Write("\b \b");
+        Console.Write("-");
+        Thread.Sleep(100);
+        Console.Write("\b \b");
+        Console.Write("\\");
+        Thread.Sleep(100);
+    }
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Controller controller = new Controller();
+        controller.ShowControls();
     }
 }
