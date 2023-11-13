ee6bbc57cc2b864dec1d54b5e47cb0e6b1b05eb4
Parkhicks <hic21003@byui.edu>
2023-11-12 21:26:47 -0700

    IDK

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -5,9 +5,18 @@ public class Goal
     bool g_status = false;
 
 
-    public Goal(int points, string name) 
+    public Goal() 
+    {
+    }
+    public virtual create_goal(int points, string name)
     {
         g_points = points;
         g_name = name;
+    }
+
+    public void Complete_goal()
+    {
+        g_status = true;
+        User.gain_points(g_points);
     }
 }
